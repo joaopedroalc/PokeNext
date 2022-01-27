43b61546d1028b2a5ca59e78bae9c9a3006046cf
joaopedroalcn <joaopedro.ufma@gmail.com>
2022-01-27 15:09:48 -0300

    finish

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,40 @@
+import Image from 'next/image';
+import CardPokemon from '../components/CardPokemon';
 import styles from '../styles/Home.module.css'
 
-export default function Home() {
+export async function getStaticProps() {
+  const maxPokemons = 251;
+  const api = 'https://pokeapi.co/api/v2/pokemon/'
+
+  const res = await fetch(`${api}/?limit=${maxPokemons}`)
+  const data = await res.json()
+
+  //add pokemon index
+  data.results.forEach((item, index) => {
+    item.id = index + 1;
+  })
+
+  return {
+    props: {
+      pokemons: data.results,
+    },
+  }
+
+}
+
+export default function Home({ pokemons }) {
   return (
-    <div className={styles.container}>
-      <h1>pokeNext</h1>
+    <div className={styles.containerHome}>
+      <div className={styles.title_container}>
+        <h1 className={styles.title}>Poke<span>Next</span></h1>
+        <Image src="/assets/pokeball.png" width="50" height="50" alt="pokeball"></Image>
+      </div>
+
+      <div className={styles.pokemon_container}>
+        {pokemons.map((pokemon) => (
+          <CardPokemon key={pokemon.id} pokemon={pokemon}></CardPokemon>
+        ))}
+      </div>
     </div>
   )
 }
